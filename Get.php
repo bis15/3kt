8f41f1761428ee6df4298a0f2d115f13d280178b
bis15 <38878918+bis15@users.noreply.github.com>
2018-05-01 15:43:47 -0800

    Update Get.php

--- Get.php
+++ Get.php
@@ -1,5 +1,5 @@
 <?php
-  echo 'Name:'.$_Get["stu_name"].'<br>'; 
-  echo 'Age:'.$_Get["stu_age"].'<br>';
-  echo 'weight:'.$_Get["stu_weight"].'<br>';
+  echo 'Name:'$_Get["stu_name"]'<br>'; 
+  echo 'Age:'$_Get["stu_age"]'<br>';
+  echo 'weight:'$_Get["stu_weight"]'<br>';
 ?>
